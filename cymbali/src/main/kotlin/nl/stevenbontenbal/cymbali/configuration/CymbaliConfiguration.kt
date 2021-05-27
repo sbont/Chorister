efcc22fa453ae183b8a832fb6f9c793ef5db678b
Steven Bontenbal <sbontenbal@growteq.nl>
2021-05-27 12:30:00 +0200

    Add YAML property file

--- cymbali/src/main/kotlin/nl/stevenbontenbal/cymbali/configuration/CymbaliConfiguration.kt
+++ cymbali/src/main/kotlin/nl/stevenbontenbal/cymbali/configuration/CymbaliConfiguration.kt
@@ -60,31 +60,7 @@ class CymbaliConfiguration {
     fun databaseInitializer(userRepository: UserRepository,
                             songRepository: SongRepository
     ) = ApplicationRunner {
-        /*val smaldini = userRepository.save(
-            User(
-            email = "smaldini@gmail.com",
-            displayName = "Stéphane",
-        )
-        )
-        songRepository.save(
-            Song(
-            title = "You'll never walk alone",
-            composer = "Lee Towers",
-            recordingUrl = "https://www.youtube.com/watch?v=OV5_LQArLa0",
-            scoreUrl = "https://sheetmusic-free.com/wp-content/uploads/2020/04/Youll-Never-Walk-Alone-Sheet-Music-PDF-Gerry-The-Pacemakers-Youll-Never-Walk-Alone-Piano-Sheet-Music-PDF-Free.png",
-            addedBy = smaldini,
-        )
-        )
-        songRepository.save(
-            Song(
-            title = "Turn Your Eyes Upon Jesus",
-            composer = "Unknown",
-            recordingUrl = "https://www.youtube.com/watch?v=OV5_LQArLa0",
-            scoreUrl = "https://sheetmusic-free.com/wp-content/uploads/2020/04/Youll-Never-Walk-Alone-Sheet-Music-PDF-Gerry-The-Pacemakers-Youll-Never-Walk-Alone-Piano-Sheet-Music-PDF-Free.png",
-            addedBy = smaldini,
-        )
-        )*/
-    }
 
+    }
 
 }
