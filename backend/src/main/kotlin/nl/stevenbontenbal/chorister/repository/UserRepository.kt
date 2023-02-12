c7ff2bb0f947be4bcb90f8604f813d43b499093a
Steven Bontenbal <stevenbontenbal@yahoo.com>
2023-02-12 16:36:10 +0100

    Fix auth filters for User

--- backend/src/main/kotlin/nl/stevenbontenbal/chorister/repository/UserRepository.kt
+++ backend/src/main/kotlin/nl/stevenbontenbal/chorister/repository/UserRepository.kt
@@ -3,6 +3,7 @@ package nl.stevenbontenbal.chorister.repository
 import nl.stevenbontenbal.chorister.model.entities.User
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.data.rest.core.annotation.RestResource
 import org.springframework.security.access.prepost.PostAuthorize
 import org.springframework.security.access.prepost.PostFilter
 
@@ -11,9 +12,9 @@ interface UserRepository : CrudRepository<User, Long> {
     @PostFilter("hasPermission(filterObject, 'read')")
     override fun findAll(): Iterable<User>
 
-    @PostAuthorize("hasPermission(filterObject, 'read')")
+    @RestResource(exported = false)
     fun findByEmail(email: String): User?
 
-    @PostAuthorize("hasPermission(filterObject, 'read')")
+    @RestResource(exported = false)
     fun findByUsername(username: String): User?
 }
