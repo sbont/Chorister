121ebcf063efef57062c5f0efec8591acdf6d45f
Steven Bontenbal <steven.bontenbal@mendix.com>
2024-05-03 11:22:05 +0200

    Add key and choir to Score

---
+++ backend/src/main/kotlin/nl/stevenbontenbal/chorister/model/entities/Key.kt
@@ -0,0 +1,16 @@
+package nl.stevenbontenbal.chorister.model.entities
+
+enum class Key(val literal: String) {
+    C("C"),
+    DB("Db"),
+    D("D"),
+    EB("Eb"),
+    E("E"),
+    F("F"),
+    GB("Gb"),
+    G("G"),
+    AB("Ab"),
+    A("A"),
+    BB("Bb"),
+    B("B"),
+}
