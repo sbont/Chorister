25d1d8d8573be6e34d381e94e9ed5208ff55320a
Steven Bontenbal <steven.bontenbal@mendix.com>
2024-05-03 11:22:05 +0200

    Add key and choir to Score

--- backend/src/main/kotlin/nl/stevenbontenbal/chorister/model/entities/Score.kt
+++ backend/src/main/kotlin/nl/stevenbontenbal/chorister/model/entities/Score.kt
@@ -1,6 +1,7 @@
 package nl.stevenbontenbal.chorister.model.entities
 
 import jakarta.persistence.*
+import nl.stevenbontenbal.chorister.interfaces.ChoirOwnedEntity
 
 @Entity
 class Score(
@@ -8,7 +9,14 @@ class Score(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CHOIR_ID")
+    override var choir: Choir?,
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "SONG_ID")
     var song: Song,
     var description: String?,
-    var fileUrl: String)
+    var fileUrl: String,
+    var key: Key?,
+) : ChoirOwnedEntity {
+    companion object
+}
