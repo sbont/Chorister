3817900bd280bef303ed3d7b4c0df4b2cd27167c
Steven Bontenbal <steven.bontenbal@mendix.com>
2025-01-26 13:43:18 +0100

    Fix more reordering issues

--- frontend/src/application/api.ts
+++ frontend/src/application/api.ts
@@ -127,7 +127,7 @@ export interface ScoresApiEndpoint extends ApiEndpoint<Score> {
 }
 
 export interface EventsApiEndpoint extends ApiEndpoint<Event> {
-    putEntries: (eventEntriesUri: Uri, entries: Array<EventEntry>) => Promise<Array<EventEntry>>
+    putEntries: (eventEntriesUri: Uri, entries: Array<EventEntry>) => Promise<void>
 }
 
 export const ApiKey: InjectionKey<Api> = Symbol("api");
