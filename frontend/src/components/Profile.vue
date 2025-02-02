fba2da865076b25d72104b07c84a93641c686d2e
Steven Bontenbal <steven.bontenbal@mendix.com>
2025-02-02 12:43:18 +0100

    Clean up Profile

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -14,24 +14,27 @@
                     <div class="field">
                         <label class="label">Name</label>
                         <div class="control">
-                            <input class="input" type="text" v-if="draftValues" v-model="draftValues.displayName"/>
+                            <input class="input" type="text" v-if="draftValues" v-model="draftValues.displayName" />
                         </div>
                     </div>
 
                     <div class="field">
                         <label class="label">Email</label>
                         <div class="control has-icons-left">
-                            <input class="input" type="email" placeholder="you@" v-if="draftValues"
-                                   v-model="draftValues.email"/>
+                            <input
+                                class="input"
+                                type="email"
+                                placeholder="you@"
+                                v-if="draftValues"
+                                v-model="draftValues.email"
+                            />
                             <span class="icon is-small is-left">
                                 <i class="fas fa-envelope"></i>
                             </span>
                         </div>
                     </div>
 
-                    <button @click="save" class="button is-link" :class="{ 'is-loading': saving }">
-                        Save changes
-                    </button>
+                    <button @click="save" class="button is-link" :class="{ 'is-loading': saving }">Save changes</button>
                 </div>
             </section>
         </div>
@@ -39,43 +42,35 @@
 </template>
 
 <script setup lang="ts">
-import { useAuth } from "@/stores/authStore";
-import { useUsers } from "@/stores/userStore";
-import { User } from "@/types";
-import { storeToRefs } from "pinia";
-import { computed, onMounted, ref } from "vue";
+import { useUsers } from "@/application/userStore";
+import { onMounted, ref } from "vue";
+import { User } from "@/entities/user";
 
-const auth = useAuth();
 const userStore = useUsers();
-const {user: authUser} = storeToRefs(auth);
 
 // State
-const user = ref<User>()
-const loading = ref(true)
-const saving = ref(false)
-const draftValues = ref<User>()
+const user = ref<User>();
+const loading = ref(true);
+const saving = ref(false);
+const draftValues = ref<User>();
 
 // Computed
 onMounted(() => {
-    console.log(auth.getUserZitadelId());
-    console.log(userStore.getCurrent());
-    userStore.getCurrent()
+    userStore
+        .getCurrent()
         .then((value) => {
             user.value = value;
             draftValues.value = value;
         })
-        .finally(() => loading.value = false);
+        .finally(() => (loading.value = false));
 });
 
 // Methods
 const save = () => {
     saving.value = true;
     user.value = draftValues.value;
-    if (draftValues.value)
-        userStore.save(draftValues.value)
-            .finally(() => saving.value = false);
-}
-
+    if (draftValues.value) userStore.save(draftValues.value).finally(() => (saving.value = false));
+};
 </script>
 <style>
 .w-40 {
