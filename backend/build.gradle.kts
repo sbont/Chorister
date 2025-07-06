ee8b44ab27a36705e77279f171c9e25350843b65
Steven Bontenbal <stevenbontenbal@yahoo.com>
2025-07-06 16:44:21 +0200

    Add CSRF fix for Spring Security 6

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -5,7 +5,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 plugins {
     val kotlinVersion = "2.0.21"
 
-    id("org.springframework.boot") version "3.4.4"
+    id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
     id("org.liquibase.gradle") version "2.2.0"
