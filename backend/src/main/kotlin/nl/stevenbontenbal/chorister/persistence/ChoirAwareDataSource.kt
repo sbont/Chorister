704ff786efae6c58b896d16017fa9b9c83047b2a
Steven Bontenbal <stevenbontenbal@yahoo.com>
2025-07-30 14:08:52 +0200

    Enable tenant logging

--- backend/src/main/kotlin/nl/stevenbontenbal/chorister/persistence/ChoirAwareDataSource.kt
+++ backend/src/main/kotlin/nl/stevenbontenbal/chorister/persistence/ChoirAwareDataSource.kt
@@ -1,12 +1,15 @@
 package nl.stevenbontenbal.chorister.persistence
 
 import nl.stevenbontenbal.chorister.application.ChoirContext
+import org.slf4j.LoggerFactory
 import org.springframework.jdbc.datasource.DelegatingDataSource
 import java.sql.Connection
 import java.sql.SQLException
 import javax.sql.DataSource
 
 class ChoirAwareDataSource(targetDataSource: DataSource) : DelegatingDataSource(targetDataSource) {
+    private val logger = LoggerFactory.getLogger(ChoirAwareDataSource::class.java)
+
     @Throws(SQLException::class)
     override fun getConnection(): Connection {
         val connection = targetDataSource!!.connection
@@ -25,10 +28,13 @@ class ChoirAwareDataSource(targetDataSource: DataSource) : DelegatingDataSource(
     fun setChoirId(connection: Connection) {
         connection.createStatement().use { sql ->
             val choirId = ChoirContext.getCurrentChoirId()
-            if (choirId != null)
+            if (choirId != null) {
+                logger.debug("Setting tenant ID = $choirId")
                 sql.execute("SET app.current_choir_id TO '$choirId'")
-            else
+            } else {
+                logger.debug("Unsetting tenant ID")
                 sql.execute("RESET app.current_choir_id")
+            }
         }
     }
 }
