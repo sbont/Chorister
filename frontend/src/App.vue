10c7f4d8da984e7e7eb4a918fac955cbe6627b8f
Steven Bontenbal <stevenbontenbal@yahoo.com>
2025-09-20 15:30:05 +0200

    Another attempt with Dart Sass

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,3 +15,15 @@ const showHeader = computed(() => {
   return !route?.meta?.hideHeader;
 })
 </script>
+
+<style>
+:root {
+    --bulma-strong-weight: var(--bulma-weight-semibold);
+    --bulma-menu-item-color: var(--bulma-white-ter);
+    --bulma-menu-item-hover-color: var(--bulma-white-bis);
+    --bulma-menu-item-hover-background-color: var(--bulma-grey-dark);
+    --bulma-menu-item-active-color: var(--bulma-white);
+    --bulma-menu-item-active-background-color: var(--bulma-purple);
+    --bulma-menu-label-color: var(--bulma-grey-lighter);
+}
+</style>
