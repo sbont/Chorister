58aaac44857b37ef9a5e98659e23f702290bb0c5
Steven Bontenbal <stevenbontenbal@yahoo.com>
2025-10-17 21:47:10 +0200

    Add entity graph and compression to cut response time by 75%

--- backend/src/main/kotlin/nl/stevenbontenbal/chorister/api/songs/SongRepository.kt
+++ backend/src/main/kotlin/nl/stevenbontenbal/chorister/api/songs/SongRepository.kt
@@ -3,6 +3,7 @@ package nl.stevenbontenbal.chorister.api.songs
 import nl.stevenbontenbal.chorister.domain.songs.Song
 import nl.stevenbontenbal.chorister.api.songs.models.SongProjection
 import org.springframework.context.annotation.Primary
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.query.Param
@@ -12,9 +13,14 @@ import org.springframework.data.rest.core.annotation.RestResource
 @Primary
 @RepositoryRestResource(excerptProjection = SongProjection::class)
 interface SongRepository : CrudRepository<Song, Long> {
+    @EntityGraph(attributePaths = ["categories", "eventEntries"])
+    override fun findAll(): Iterable<Song>
+
     @RestResource(path = "bycategory", rel = "bycategory")
+    @EntityGraph(attributePaths = ["categories", "eventEntries"])
     fun findAllByCategories_id(@Param("id") categoryId: Long): Iterable<Song>
 
     @Query("select s from Song s join Event l where l.id = :id")
+    @EntityGraph(attributePaths = ["categories", "eventEntries"])
     fun byevent(@Param("id") eventId: Long): Iterable<Song>
 }
