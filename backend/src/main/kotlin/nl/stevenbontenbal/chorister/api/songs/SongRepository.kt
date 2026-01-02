8ad51a948839cbc3d6ee72298a3c89a2a62870f3
Steven Bontenbal <stevenbontenbal@yahoo.com>
2026-01-02 18:19:40 +0100

    Add role based access to /songs

--- backend/src/main/kotlin/nl/stevenbontenbal/chorister/api/songs/SongRepository.kt
+++ backend/src/main/kotlin/nl/stevenbontenbal/chorister/api/songs/SongRepository.kt
@@ -1,7 +1,7 @@
 package nl.stevenbontenbal.chorister.api.songs
 
-import nl.stevenbontenbal.chorister.domain.songs.Song
 import nl.stevenbontenbal.chorister.api.songs.models.SongProjection
+import nl.stevenbontenbal.chorister.domain.songs.Song
 import org.springframework.context.annotation.Primary
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.Query
@@ -9,12 +9,34 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.data.rest.core.annotation.RestResource
+import org.springframework.security.access.prepost.PreAuthorize
 
 @Primary
 @RepositoryRestResource(excerptProjection = SongProjection::class)
 interface SongRepository : CrudRepository<Song, Long> {
     @EntityGraph(attributePaths = ["categories", "eventEntries"])
     override fun findAll(): Iterable<Song>
+
+    @PreAuthorize("hasRole('EDITOR')")
+    override fun <S : Song> save(entity: S): S
+
+    @PreAuthorize("hasRole('EDITOR')")
+    override fun <S : Song> saveAll(entities: Iterable<S>): Iterable<S>
+
+    @PreAuthorize("hasRole('EDITOR')")
+    override fun delete(entity: Song)
+
+    @PreAuthorize("hasRole('EDITOR')")
+    override fun deleteById(id: Long)
+
+    @PreAuthorize("hasRole('MANAGER')")
+    override fun deleteAll()
+
+    @PreAuthorize("hasRole('MANAGER')")
+    override fun deleteAllById(ids: Iterable<Long?>)
+
+    @PreAuthorize("hasRole('MANAGER')")
+    override fun deleteAll(entities: Iterable<Song?>)
 
     @RestResource(path = "bycategory", rel = "bycategory")
     @EntityGraph(attributePaths = ["categories", "eventEntries"])
