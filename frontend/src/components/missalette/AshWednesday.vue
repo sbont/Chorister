5e5278822ad30157b163b04f8b1a6287a8ed981b
Steven Bontenbal <stevenbontenbal@yahoo.com>
2026-02-13 13:49:42 +0100

    Add ash wednesday order of mass drafts

---
+++ frontend/src/components/missalette/AshWednesday.vue
@@ -0,0 +1,203 @@
+The Introductory Rites
+
+Greeting
+
+Priest
+In the name of the Father, and of the Son, and of the Holy Spirit.
+
+People
+Amen.
+
+Priest
+The grace of our Lord Jesus Christ, and the love of God, and the communion of the Holy Spirit be with you all.
+Or:
+Grace to you and peace from God our Father and the Lord Jesus Christ.
+Or:
+The Lord be with you.
+
+People
+And with your spirit.
+
+The Collect
+
+Priest
+Let us pray.
+And all pray in silence with the Priest for a moment.
+Then the Priest, with hands extended, says the Collect prayer, at the end of which the people acclaim:
+Amen.
+
+The Liturgy of the Word
+
+First Reading
+
+{{ firstReading }}
+
+Lector
+The word of the Lord.
+People
+Thanks be to God.
+
+Psalm
+
+{{ psalm }}
+
+Second Reading
+
+{{ secondReading }}
+
+Lector
+The word of the Lord.
+People
+Thanks be to God.
+
+Gospel
+
+{{ gospelAcclamation }}
+
+{{ gospelAcclamationVerse }}
+
+{{ gospelAcclamation }}
+
+Priest
+The Lord be with you.
+People
+And with your spirit.
+Priest
+A reading from the holy Gospel according to N.
+People
+Glory to you, O Lord.
+
+{{ gospelReading }}
+
+Priest
+The Gospel of the Lord.
+People
+Praise to you, Lord Jesus Christ.
+
+The Homily 
+
+The Prayer of the Faithful
+
+After each intention there is a pause while the faithful pray.
+Lord, in your mercy.
+Hear our prayer.
+
+The Liturgy of the Eucharist
+
+Priest
+Pray, brothers and sisters, that my sacrifice and yours may be acceptable to God, the almighty Father. 
+People
+May the Lord accept the sacrifice at your hands for the praise and glory of his name, for our good and the good of all his holy Church.
+
+Then the Priest says the Prayer over the Offerings, at the end of which the people acclaim:
+Amen.
+
+The Eucharistic Prayer
+
+Priest
+The Lord be with you.
+People
+And with your spirit.
+Priest
+Lift up your hearts.
+People
+We lift them up to the Lord.
+Priest
+Let us give thanks to the Lord our God.
+People
+It is right and just.
+
+The Preface
+
+Holy, Holy, Holy Lord God of hosts.
+Heaven and earth are full of your glory.
+Hosanna in the highest.
+Blessed is he who comes in the name
+of the Lord.
+Hosanna in the highest.
+
+After the words of Consecration the priest says:
+The mystery of faith.
+People
+We proclaim your Death, O Lord, and profess your Resurrection until you come again. 
+Or:
+When we eat this Bread and drink this Cup, we proclaim your Death, O Lord, until you come again. 
+Or:
+Save us, Saviour of the world, for by your Cross and Resurrection you have set us free.
+
+Priest
+Through him, and with him, and in him,
+O God, almighty Father,
+in the unity of the Holy Spirit,
+all glory and honour is yours,
+for ever and ever.
+People
+Amen.
+
+The Communion Rite
+
+The Lord’s Prayer
+Priest
+At the Saviour’s command and formed by divine teaching, we dare to say:
+People
+Our Father, who art in heaven,
+hallowed be thy name;
+thy kingdom come,
+thy will be done
+on earth as it is in heaven.
+Give us this day our daily bread,
+and forgive us our trespasses,
+as we forgive those who trespass against us;
+and lead us not into temptation,
+but deliver us from evil.
+Priest
+Deliver us, Lord, we pray, from every evil…
+and the coming of our Saviour, Jesus Christ.
+People
+For the kingdom,
+the power and the glory are yours
+now and for ever.
+Priest
+Lord Jesus Christ,
+who said to your Apostles…
+Who live and reign for ever and ever.
+People
+Amen.
+Priest
+The peace of the Lord be with you always.
+People
+And with your spirit.
+Priest
+Let us offer each other the sign of peace.
+
+All offer one another the customary sign of peace.
+
+Breaking of the Bread
+
+
+Lamb of God, you take away the sins of the world, have mercy on us.
+Lamb of God, you take away the sins of the world, have mercy on us.
+Lamb of God, you take away the sins of the world, grant us peace.
+
+Communion
+
+Priest
+Behold the Lamb of God, behold him who takes away the sins of the world.
+Blessed are those called to the supper of the Lamb.
+People
+Lord, I am not worthy that you should enter under my roof, but only say the word and my soul shall be healed.
+
+The communicants come forward in reverent procession, and make a preparatory act of reverence by bowing their head in honour of Christ’s presence in the Sacrament.
+
+Priest
+The Body (Blood) of Christ.
+communicant
+Amen.
+
+Prayer after Communion 
+
+Priest
+Let us pray.
+
+All pray in silence. Then the Priest says the Prayer after Communion, at the end of which the people acclaim:
+Amen.
---
+++ frontend/src/components/missalette/SundayInLent.vue
@@ -0,0 +1,113 @@
+The Introductory Rites
+
+Greeting
+
+Priest
+In the name of the Father, and of the Son, and of the Holy Spirit.
+
+People
+Amen.
+
+Priest
+The grace of our Lord Jesus Christ, and the love of God, and the communion of the Holy Spirit be with you all.
+Or:
+Grace to you and peace from God our Father and the Lord Jesus Christ.
+Or:
+The Lord be with you.
+
+People
+And with your spirit.
+
+Penitential Act
+
+People
+I confess to almighty God and to you, my brothers and sisters, that I have greatly sinned, in my thoughts and in my words, in what I have done and in what I have failed to do, 
+through my fault, through my fault, through my most grievous fault;
+therefore I ask blessed Mary ever-Virgin, all the Angels and Saints, and you, my brothers and sisters, to pray for me to the Lord our God.
+Priest
+May almighty God have mercy on us, forgive us our sins, and bring us to everlasting life.
+People
+Amen.
+
+Lord, have mercy. Or: Kyrie, eleison.
+Lord, have mercy. Or: Kyrie, eleison.
+Christ, have mercy. Or: Christe, eleison.
+Christ, have mercy. Or: Christe, eleison.
+Lord, have mercy. Or: Kyrie, eleison.
+Lord, have mercy. Or: Kyrie, eleison.
+
+Priest
+Let us pray.
+And all pray in silence with the Priest for a moment.
+Then the Priest, with hands extended, says the Collect prayer, at the end of which the people acclaim:
+Amen.
+
+The Liturgy of the Word
+
+First Reading
+
+{{ firstReading }}
+
+Lector
+The word of the Lord.
+People
+Thanks be to God.
+
+Psalm
+
+{{ psalm }}
+
+Second Reading
+
+{{ secondReading }}
+
+Lector
+The word of the Lord.
+People
+Thanks be to God.
+
+Gospel
+
+{{ gospelAcclamation }}
+
+{{ gospelAcclamationVerse }}
+
+{{ gospelAcclamation }}
+
+Priest
+The Lord be with you.
+People
+And with your spirit.
+Priest
+A reading from the holy Gospel according to N.
+People
+Glory to you, O Lord.
+
+{{ gospelReading }}
+
+Priest
+The Gospel of the Lord.
+People
+Praise to you, Lord Jesus Christ.
+
+The Homily 
+
+The Creed
+
+The Apostles’ Creed
+
+I believe in God, the Father almighty, Creator of heaven and earth, and in Jesus Christ, his only Son, our Lord, 
+who was conceived by the Holy Spirit, born of the Virgin Mary, 
+suffered under Pontius Pilate, was crucified, died and was buried; he descended into hell; on the third day he rose again from the dead; 
+he ascended into heaven, and is seated at the right hand of God the Father almighty; from there he will come to judge the living and the dead. 
+I believe in the Holy Spirit, the holy catholic Church, the communion of saints, the forgiveness of sins, the resurrection of the body, and life everlasting. 
+Amen.
+
+The Prayer of the Faithful
+
+After each intention there is a pause while the faithful pray.
+Lord, in your mercy.
+Hear our prayer.
+
+The Liturgy of the Eucharist
+
