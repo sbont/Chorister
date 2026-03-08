22a6d6a32586aa6e1337535d0127c81f59b5d51a
Steven Bontenbal <stevenbontenbal@yahoo.com>
2026-03-08 18:34:41 +0100

    Add mobile events menu

--- frontend/src/components/events/Planning.vue
+++ frontend/src/components/events/Planning.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="columns is-gapless">
-        <div id="menu" class="column is-one-third-tablet is-one-fifth-desktop has-background-grey-darker">
+        <div id="menu" class="column is-hidden-mobile	is-one-third-tablet is-one-fifth-desktop has-background-grey-darker">
             <aside class="menu p-2">
                 <p v-if="authStore.userCan('create', 'event')" class="menu-label">Create</p>
                 <ul v-if="authStore.userCan('create', 'event')" class="menu-list">
@@ -49,6 +49,11 @@
                 </ul>
             </aside>
         </div>
+        <div class="mobile-menu is-hidden-tablet">
+          <div class="column">
+            <Select v-model="selectedEvent" :options="options" option-group-label="label" option-group-children="items" option-label="label" option-value="value" placeholder="Select an event" class="w-full md:w-56" @change="selectEvent" />
+          </div>
+        </div>
         <div class="column">
             <!-- this construction is necessary to rerender between different routes using the same view -->
             <router-view v-slot="{ Component, route }">
@@ -62,12 +67,28 @@
 import { useEvents } from "@/application/eventStore";
 import { storeToRefs } from "pinia";
 import { useAuth } from "@/application/authStore";
+import Select, { SelectChangeEvent } from 'primevue/select';
+import { computed, ref } from "vue";
+import { useRouter } from "vue-router";
 
 const authStore = useAuth();
 const eventStore = useEvents();
 eventStore.initialize();
+const router = useRouter();
 
 // State
+const selectedEvent = ref<number>();
 const { futureEvents, pastEvents, isLoading } = storeToRefs(eventStore);
+const options = computed(() => ([{
+  label: "Future events",
+  items: futureEvents.value.map(e => ({ "label": e.name, "value": e.id }))
+}, {
+  label: "Past events",
+  items: pastEvents.value.map(e => ({ "label": e.name, "value": e.id }))
+}]));
+
+function selectEvent(evt: SelectChangeEvent) {
+  router.push({ name: 'Event', params: { id: evt.value } })
+}
 
 </script>
